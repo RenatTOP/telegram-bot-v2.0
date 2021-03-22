2e2a3705c22c3a93ed87ec27ef388d41bd5a301c
RenatTOP <renatseverski@gmail.com>
2021-03-22 09:54:24 +0200

    fix test

--- app/keyboards/inline/products_buttons.py
+++ app/keyboards/inline/products_buttons.py
@@ -113,7 +113,7 @@ add_edit_prod = InlineKeyboardMarkup(
 # ?edit Product buttons
 
 
-async def products_list(products: list, pages: int, keyboard):
+async def products_list(products: list, pages: int):
     prod_list = InlineKeyboardMarkup()
     async for prod in products:
         _id = prod["_id"]
@@ -140,7 +140,7 @@ async def products_list(products: list, pages: int, keyboard):
             callback_data=cd.prod_nav_list_callback.new(pages=f"{pages_next}"),
         )
     )
-    if keyboard == "admin":
+    if 'admin' == "admin":
         prod_list.add(await help_kb.back("products"))
     else:
         prod_list.add(await help_kb.back("user_menu"))
