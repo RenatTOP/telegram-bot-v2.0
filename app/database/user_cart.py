e9be0d6b1cc94ebe6b5e3df3333acdb532328c5b
RenatTOP <renatseverski@gmail.com>
2021-03-24 10:35:20 +0200

    fix

--- app/database/user_cart.py
+++ app/database/user_cart.py
@@ -5,6 +5,7 @@ from app.database.database import products
 
 async def add_product_to_cart(user_id: int, prod_id: str):
     number_prod = await users.find_one({"userId": user_id}, {f"cart.{prod_id}"})
+    print(number_prod)
     number_prod = number_prod["cart"][f"{prod_id}"]
     number_prod += 1
     await users.update_one({"userId": user_id}, {"$set": {f"cart.{prod_id}": number_prod}})
@@ -12,7 +13,6 @@ async def add_product_to_cart(user_id: int, prod_id: str):
 
 async def del_product_from_cart(user_id: int, prod_id: str):
     number_prod = await users.find_one({"userId": user_id}, {f"cart.{prod_id}"})
-    print(number_prod)
     number_prod = number_prod["cart"][f"{prod_id}"]
     number_prod -= 1
     await users.update_one({"userId": user_id}, {"$set": {f"cart.{prod_id}": number_prod}})
