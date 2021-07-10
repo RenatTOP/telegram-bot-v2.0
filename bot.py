144f121899c505db1bc91ca69d7a74cf40dd506b
RenatTOP <renatseverski@gmail.com>
2021-07-10 23:11:14 +0300

    fix

--- bot.py
+++ bot.py
@@ -27,9 +27,19 @@ async def on_startup(request) -> web.Response:
     Bot.set_current(bot)
     await dp.bot.delete_webhook()
     await dp.bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
-    # request_body_dict = await request.json()
-    # update = types.Update(**(request_body_dict))
-    # await dp.process_updates([update])
+    request_body_dict = WEBHOOK_URL
+    update = types.Update(**(request_body_dict))
+    await dp.process_updates([update])
+    return web.Response(status=200)
+
+
+async def execute(request) -> web.Response:
+    # Bot.set_current(bot)
+    # await dp.bot.delete_webhook()
+    # await dp.bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+    request_body_dict = await request.json()
+    update = types.Update(**(request_body_dict))
+    await dp.process_updates([update])
     return web.Response(status=200)
 
 
