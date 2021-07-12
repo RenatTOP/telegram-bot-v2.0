adc127ed9d7d6b07ad67c42424cd4b024cd3c180
RenatTOP <renatseverski@gmail.com>
2021-07-12 20:50:15 +0300

    fix states v4.2

--- app/handlers/products/add_product.py
+++ app/handlers/products/add_product.py
@@ -25,8 +25,7 @@ from app.handlers.products.prod_helper import string_kinds, string_confirm
 async def add_product(call: CallbackQuery, state: FSMContext):
     chat_id, message_id = await call_chat_and_message(call)
     await state_check(state)
-    this_state = dp.current_state(user=call.from_user.id)
-    await this_state.set_state(Product.first())
+    await state.set_state(Product.waiting_for_label.set())
     text = f"Введіть назву товару"
     kb_prod_back = InlineKeyboardMarkup()
     kb_prod_back.add(back("products"))
